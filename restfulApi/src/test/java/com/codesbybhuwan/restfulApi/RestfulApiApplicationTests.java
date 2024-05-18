d956d88f349a8470048375f6aa0091e0fac92b1c
CodesBy-Bhuwan <bhuwan.thadarai817@gamil.com>
2024-05-18 23:11:25 +0545

    Initailization BlogRestfulApi

---
+++ restfulApi/src/test/java/com/codesbybhuwan/restfulApi/RestfulApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codesbybhuwan.restfulApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
