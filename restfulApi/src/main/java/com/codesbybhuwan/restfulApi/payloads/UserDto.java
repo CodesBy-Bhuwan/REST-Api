a376546a9a9e7d3d34ae2943aa0856cc260221d4
CodesBy-Bhuwan <bhuwan.thadarai817@gamil.com>
2024-05-21 16:53:59 +0545

    Hibernate-Validator

--- restfulApi/src/main/java/com/codesbybhuwan/restfulApi/payloads/UserDto.java
+++ restfulApi/src/main/java/com/codesbybhuwan/restfulApi/payloads/UserDto.java
@@ -1,6 +1,7 @@
 package com.codesbybhuwan.restfulApi.payloads;
 
 
+import jakarta.validation.constraints.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -14,9 +15,21 @@ import java.util.stream.Collector;
 public class UserDto {
 
     private int id;
+//    Hibernate Validator annoted to enable annotation we will enable from Controller
+    @NotEmpty
+//    Instead of NotNull NotEmpty would be better option since it will check both the condtion
+    @Size(min = 3, message = "Username must have atleast 3 characters")
     private String name;
+
+    @Email(message = "Email address is not valid")
     private String email;
+
+    @NotEmpty
+    @Size(min = 8, message="The password must have atleast 8 character")
+    @Pattern(regexp = "([a-zA-Z0-9]{4}$)")
     private String password;
+
+    @NotEmpty
     private String about;
 
 }
