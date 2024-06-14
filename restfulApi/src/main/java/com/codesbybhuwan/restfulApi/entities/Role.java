29c38bb29090291a22e6b5b006c032b5cc84d632
CodesBy-Bhuwan <bhuwan.thadarai817@gamil.com>
2024-06-14 22:04:41 +0545

    Initializing Validation&&Authentication

---
+++ restfulApi/src/main/java/com/codesbybhuwan/restfulApi/entities/Role.java
@@ -0,0 +1,18 @@
+package com.codesbybhuwan.restfulApi.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data //this is provide us Getter and Setter
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+
+}
